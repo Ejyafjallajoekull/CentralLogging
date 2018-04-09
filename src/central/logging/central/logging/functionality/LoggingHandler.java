54f4314949cecf4c6e3ab84913eb06f1751e6ebb
Planters <Planters@a.a>
2018-04-09 14:59:36 +0200

    Updated Version Number
    Incremented version number to reflect the latest bug fix.

--- src/central/logging/central/logging/functionality/LoggingHandler.java
+++ src/central/logging/central/logging/functionality/LoggingHandler.java
@@ -20,7 +20,7 @@ public class LoggingHandler {
 	/**
 	 * The version number of this program.
 	 */
-	public static final String VERSION = "1.0.0.1";
+	public static final String VERSION = "1.0.0.2";
 	/**
 	 * The centralised logger to be used.
 	 */
